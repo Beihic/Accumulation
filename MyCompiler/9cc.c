db3e1ecaf1ad1f5cf659385ec8a3a2f429e1d65f
Beihic <Beihic.Pgm@gmail.com>
2021-08-01 21:55:58 +0900

    step2の完了!! #22

--- MyCompiler/9cc.c
+++ MyCompiler/9cc.c
@@ -3,15 +3,34 @@
 
 int main(int argc, char **argv) {
 	if(argc != 3) {
-		fprintf(stderr, "引数の個数が正しくありません\n");
+		fprintf(stderr, "Error:Need correct number of argv\n");
 		return 1;
 	}
+	char *p = argv[1];
 	FILE *fp;
+
        	fp = fopen(argv[2], "w");	
 	fprintf(fp, ".intel_syntax noprefix\n");
 	fprintf(fp, ".globl main\n");
 	fprintf(fp, "main:\n");
-	fprintf(fp, "  mov rax, %d\n", atoi(argv[1]));
+	fprintf(fp, "	mov rax, %ld\n", strtol(p, &p, 10));
+
+	while (*p){
+		if(*p == '+'){
+			p++;
+			fprintf(fp, "	add rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		if(*p == '-'){
+			p++;
+			fprintf(fp, "	sub rax, %ld\n", strtol(p, &p, 10));
+			continue;
+		}
+
+		fprintf(stderr, "Not expected Character '%c'\n", *p);
+		return 1;
+	}
 	fprintf(fp, "  ret\n");
 	fclose(fp);
 	return 0;
